424d8eef7d7e9669fdd97062aa545a60b38dfa8e
EC2 Default User <ec2-user@ip-172-31-19-43.us-west-2.compute.internal>
2023-03-27 02:13:43 +0000

    snapshot creation

---
+++ create_snapshot.py
@@ -0,0 +1,37 @@
+import boto3
+import sys
+
+# Set up the EC2 client
+ec2 = boto3.client('ec2')
+
+# Define the volume ID and snapshot description
+volume_id = sys.argv[1] #arguement 1 is volume id 
+snapshot_description = sys.argv[2] #Arguemtn 2 is 'YOUR_SNAPSHOT_DESCRIPTION'
+rentention_period = sys.argv[3]
+
+# Define the tags in a dictionary
+tags = {
+    'Retention Period': rentention_period
+}
+
+# Create the snapshot
+response = ec2.create_snapshot(
+    VolumeId=volume_id,
+    Description=snapshot_description
+)
+
+# Get the snapshot ID
+snapshot_id = response['SnapshotId']
+print(f"Snapshot {snapshot_id} created.")
+
+# Wait for the snapshot to complete
+#waiter = ec2.get_waiter('snapshot_completed')
+#waiter.wait(SnapshotIds=[snapshot_id])
+
+# Tag the snapshot
+ec2.create_tags(
+    Resources=[snapshot_id],
+    Tags=[{'Key': key, 'Value': value} for key, value in tags.items()]
+)
+#print(f"Snapshot {snapshot_id} tagged with {tags}.")
+
